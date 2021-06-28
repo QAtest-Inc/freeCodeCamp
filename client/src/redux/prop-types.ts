6fc2438f59acb28aad300615ee6d735f6ed6a072
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-06-28 11:25:02 -0700

    feat: add campfire mode
    fix: resolve lint issues
    
    feat: add sound to editor
    
    fix: restore flash messages
    
    fix: linter issues
    
    fix: obey sound setting
    
    Update the editor to obey the camper's sound setting.
    
    chore: apply suggestions from code review
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
    
    fix: use @types/store
    
    fix: linter issues
    
    feat: simplify sound saga
    
    Update client/src/redux/sound-mode-saga.js
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
    
    fix: missing bracket
    
    chore: use new s3 bucket
    
    fix: lint
    
    fix: import only needed bits
    
    fix: remove from navbar
    
    (was intermittently broken here anyway)
    
    fix: dynamic imports?
    
    fix: more dynamic imports
    
    fix: tweak theme logic
    
    chore: boolean | undefined
    
    fix: dns

--- client/src/redux/prop-types.ts
+++ client/src/redux/prop-types.ts
@@ -118,6 +118,7 @@ export const User = PropTypes.shape({
     })
   ),
   sendQuincyEmail: PropTypes.bool,
+  sound: PropTypes.bool,
   theme: PropTypes.string,
   twitter: PropTypes.string,
   username: PropTypes.string,
@@ -294,6 +295,7 @@ export type UserType = {
   };
   progressTimestamps: Array<unknown>;
   sendQuincyEmail: boolean;
+  sound: boolean;
   theme: string;
   twitter: string;
   username: string;
