1b126cdfecff5513715468a18635c2c89f2dc467
Yuriy B <89322162+qause@users.noreply.github.com>
2021-10-19 18:49:34 +0300

    Update test_case_001.js

--- cypress/integration/learn/quality-assurance/test_case_001.js
+++ cypress/integration/learn/quality-assurance/test_case_001.js
@@ -1,17 +1,25 @@
-describe('test_name', function () {
-  it('what_it_does', function () {
-    cy.viewport(1717, 848);
+describe('test_name', function() {
 
-    cy.visit('https://8000-azure-mule-w1x3dedo.ws-eu17.gitpod.io/learn');
+ it('what_it_does', function() {
 
-    cy.get(
-      'header > #universal-nav > .universal-nav-middle > #universal-nav-logo > svg'
-    ).click();
+    cy.viewport(1706, 1225)
+ 
+    cy.visit('https://8000-azure-mule-w1x3dedo.ws-eu17.gitpod.io/learn/quality-assurance/quality-assurance-and-testing-with-chai/test-if-a-variable-or-function-is-defined')
+ 
+    cy.get('header > #universal-nav > .universal-nav-middle > #universal-nav-logo > svg').click()
+ 
+    cy.get('.map-ui > ul > li:nth-child(6) > .btn > div').click()
+ 
+    cy.get('.block-ui > .block > .map-challenges-ul > #learn-how-javascript-assertions-work > a').click()
+ 
+    cy.get('#dynamic-front-end-form > div > .col-xs-12 > .form-group > #solution').click()
+ 
+    cy.get('#dynamic-front-end-form > div > .col-xs-12 > .form-group > #solution').type('https://boilerplate-mochachai-2.qause.repl.co')
+ 
+    cy.get('.row > .col-md-8 > #dynamic-front-end-form > div > .btn').click()
+ 
+    cy.get('.in > .challenge-success-modal > .modal-content > .modal-footer > .btn').click()
+ 
+ })
 
-    cy.get('.map-ui > ul > li:nth-child(6) > .btn > div').click();
-
-    cy.get(
-      '.block-ui > .block > .map-challenges-ul > #learn-how-javascript-assertions-work > a'
-    ).click();
-  });
-});
+})
